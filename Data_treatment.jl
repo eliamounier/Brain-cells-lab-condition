a688fd185f44da52bf751bca1565124177463c0f
eliamounier <elia.mounier-poulat@epfl.ch>
2022-12-07 16:00:52 +0100

    removl of useless packages and try new stuff

--- Data_treatment.jl
+++ Data_treatment.jl
@@ -1,13 +1,18 @@
 using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using Plots, OpenML, DataFrames, CSV, MLCourse, Random, Statistics, MLJ, MLJLinearModels, Distributions
+using DataFrames, CSV ,Statistics
 
 train_data = CSV.read("DATA/train.csv", DataFrame)
 coerce!(train_data, :labels => Multiclass)
 #dropmissing was tested and output is of same size -> no missing Data
+dropmissing!(train_data);
 X = select(train_data, Not(:labels))
 y = train_data.labels
+#removal of constant parameters:
 X_const = X[:, std.(eachcol(X)) .!= 0]
 
+#removal of perfectly correlated redundant parameters:
+findall(≈(1), cor(Matrix(X_const))) |> # find all indices with correlation ≈ 1
+idxs -> filter(x -> x[1] > x[2], idxs)
 #corr = findall(≈(1), cor(Matrix(X_const))) |> idxs -> filter(x -> x[1] > x[2], idxs)
 #a = 1:size(corr)[1]
 #corr_colnames = [names(X_const)[i] for i in a]
@@ -19,7 +24,8 @@ X_const = X[:, std.(eachcol(X)) .!= 0]
 #dataX = X_const
 #datay = y
 
-
-test_data = CSV.read("DATA/test.csv", DataFrame)
-CSV.write("DATA/datay.csv", y)
+#test_data = CSV.read("DATA/test.csv", DataFrame)
 CSV.write("DATA/dataX.csv", X_const)
+
+
+#
--- non_linear_methods.jl
+++ non_linear_methods.jl
@@ -1,28 +1,45 @@
 # This folder is for non-linear methods
 using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using DataFrames, MLJ, MLJLinearModels, MLCourse, Random, Distributions, Plots, MLJFlux, Flux, OpenML, MLJDecisionTreeInterface, CSV
-
+using DataFrames, CSV
 
 train_input = CSV.read("DATA/dataX.csv", DataFrame)[1:100, :]
 train_class = CSV.read("DATA/datay.csv", DataFrame)[1:100, :]
-
-
 test_input = CSV.read("DATA/test.csv", DataFrame)
 
-#FOREST TREE
+#FOREST TREE CLASSIFIER
+using MLJDecisionTreeInterface
 #machine for a random forest with 500 trees
-mach_forest_tree = machine(RandomForestClassifier(n_trees = 50), train_input, train_class) |> fit!
+mach_forest_tree = machine(RandomForestClassifier(n_trees = 50), train_input, train_class) |> fit!;
 #prediction on the test set with random forest:
 prediction_forest_tree = String.(predict_mode(mach_forest_tree, test_input))
 df_predict_tree = Dataframe(id :: 1:3093, prediction_forest_tree)
-
-
-accuracy = mean(prediction_forest_tree .== test_output)
-confusion_matrix(prediction_forest_tree, test_output)
-
-
 CSV.write("./predict_foresttree.csv", prediction_forest_tree)
 
+#accuracy = mean(prediction_forest_tree .== test_output)
+#confusion_matrix(prediction_forest_tree, test_output)
+
+
 #NEURON NETWORK CLASSIFIER
-mach_neuron_network_classifier = machine(NeuralNetworkClassifier(builder = MLJFlux.Short(n_hidden = 128, dropout = 0.1, σ = relu),
+using MLJ, MLJFlux, Flux
+#machine for neuron network classifier
+mach_neuron_network = machine(NeuralNetworkClassifier(builder = MLJFlux.Short(n_hidden = 128, dropout = 0.1, σ = relu),
                                                                 batch_size = 32, epochs = 30),train_input, train_class)|> fit!;
+#prediction on test set
+prediction_neuron_network = String.(predict_mode(mach_neuron_network, test_input))
+
+df_neuron_network = Dataframe(id :: 1:3093, prediction_neuron_network)
+CSV.write("./predict_neuronnetwork.csv", prediction_neuron_network)
+
+#IMPROVEMENT OF NEURON NETWORK CLASSIFIER WITH MULTILAYER PERCEPTRON
+
+
+
+
+#CLUSTERING ?==> or for visualisation
+using MLJ, MLJClusteringInterface, MLJMultivariateStatsInterface, Random
+
+##K-means clustering
+#machine with k = 3 clusters :
+#Random.seed!(10); mach_k_means_clusters = fit!(machine(KMeans(k = 3), train_input));
+#prediction with k-means clustering on training data
+#prediction_k_means_clusters = MLJ.predict(mach_k_means_clusters, train_imput);
