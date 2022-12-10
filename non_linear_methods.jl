34db405880ec51f6328f2aed43fee853a3c86b5b
eliamounier <elia.mounier-poulat@epfl.ch>
2022-12-10 23:28:23 +0100

    +

--- non_linear_methods.jl
+++ non_linear_methods.jl
@@ -3,25 +3,28 @@ using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
 using DataFrames, MLJ, MLJLinearModels, MLCourse, Random, Distributions, Plots, MLJFlux, Flux, OpenML, MLJDecisionTreeInterface, CSV
 
 
-train_input = CSV.read("DATA/dataX.csv", DataFrame)[1:100, :]
-train_class = CSV.read("DATA/datay.csv", DataFrame)[1:100, :]
+train_input = CSV.read("DATA/trainX.csv", DataFrame)
+train_class = CSV.read("DATA/trainy.csv", DataFrame)
 
+using Serialization
+soe = deserialize("DATA/trainlabels.dat")
+soe1 = categorical(soe, levels = ["KAT5", "eGFP", "CBP"], ordered = true)
 
-test_input = CSV.read("DATA/test.csv", DataFrame)
+test_input = CSV.read("DATA/testX.csv", DataFrame)
 
 #FOREST TREE
 #machine for a random forest with 500 trees
-mach_forest_tree = machine(RandomForestClassifier(n_trees = 50), train_input, train_class) |> fit!
+mach_forest_tree = machine(RandomForestClassifier(n_trees = 3000), train_input, soe1) |> fit!
 #prediction on the test set with random forest:
 prediction_forest_tree = String.(predict_mode(mach_forest_tree, test_input))
-df_predict_tree = Dataframe(id :: 1:3093, prediction_forest_tree)
+df_predict_tree = DataFrame(id = 1:3093, prediction = prediction_forest_tree)
 
 
 accuracy = mean(prediction_forest_tree .== test_output)
 confusion_matrix(prediction_forest_tree, test_output)
 
 
-CSV.write("./predict_foresttree.csv", prediction_forest_tree)
+CSV.write("./predict_foresttree_bis.csv", df_predict_tree)
 
 #NEURON NETWORK CLASSIFIER
 mach_neuron_network_classifier = machine(NeuralNetworkClassifier(builder = MLJFlux.Short(n_hidden = 128, dropout = 0.1, σ = relu),
