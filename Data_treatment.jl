7d606c17aa3d1f76313084cea8f61487c3b8023d
eliamounier <elia.mounier-poulat@epfl.ch>
2022-12-11 18:58:43 +0100

    good

--- Data_treatment.jl
+++ Data_treatment.jl
@@ -1,15 +1,20 @@
 using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using Plots, OpenML, DataFrames, CSV, MLCourse, Random, Statistics, MLJ, MLJLinearModels, Distributions
+using DataFrames, CSV, Statistics, MLJ, Serialization
 
+#importing datasets
 train_data = CSV.read("DATA/train.csv", DataFrame)
 test_data = CSV.read("DATA/test.csv", DataFrame)
-# coerce!(train_data, :labels => Multiclass)
+train_output = categorical(train_data.labels, levels = ["KAT5", "eGFP", "CBP"], ordered = true)
+
 #dropmissing was tested and output is of same size -> no missing Data
+
+#REMOVAL OF CONSTANT PREDICTORS
 X = select(train_data, Not(:labels))
 X_totreat = vcat(X, test_data)
 y = train_data.labels
 X_const = X_totreat[:, std.(eachcol(X)) .!= 0]
 
+#REMOVAL OF COORELATED PREDICTORS
 corr = findall(≈(1), cor(Matrix(X_const))) |> idxs -> filter(x -> x[1] > x[2], idxs)
 a = 1:size(corr)[1]
 corr_indexes1 = [corr[i][1] for i in a]
@@ -17,11 +22,11 @@ corr_names1 = [names(X_const)[j] for j in corr_indexes1]
 corr_names1_wout = unique(corr_names1)
 X_cleaned = select(X_const, Not(corr_names1_wout))
 
+#back into exploitable form
+dataX = X_cleaned[1:5000, :]
+dataT = X_cleaned[5001:8093, :]
 
-dataX = X_const[1:5000, :]
-dataT = X_const[5001:8093, :]
-datay = y
-
-
-CSV.write("DATA/datay.csv", y)
-CSV.write("DATA/dataX.csv", X_const)
+#Saving new datasets
+serialize("DATA/trainlabels.dat", train_output)
+CSV.write("DATA/trainX.csv", dataX)
+CSV.write("DATA/testX.csv", dataT)
